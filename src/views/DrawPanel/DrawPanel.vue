3762563850765ee214eeb7d0a3546a34ec519aec
Lele <1524078449@qq.com>
2022-08-04 13:31:09 +0800

    111

--- src/views/DrawPanel/DrawPanel.vue
+++ src/views/DrawPanel/DrawPanel.vue
@@ -18,8 +18,7 @@
     >
       {{ item.data }}
     </div> -->
-    <div class="contain">
-    </div>
+    <div class="contain"></div>
     <!-- <div style="width: 200px; height: 100px">
         <button style="width: 100px; height: 100px" @click="cutit">点我删除</button>
         <button style="width: 100px; height: 100px" @click="addit">点我增加</button>
@@ -29,56 +28,90 @@
 
 <script>
 export default {
-  name:'DrawPanel',
-  props:{
-    dataList:{
-        type:Array
-    }
+  name: "DrawPanel",
+  props: {
+    dataList: {
+      type: Array,
+    },
   },
-  methods:{
-        // cutit(){
-        //     let temp = this.dataList;
-        //     temp.pop();
-        //     this.dataList = temp;
-            
-        // },
-        // addit(){
-        //     let temp = this.dataList;
-        //     let tempIndex = this.dataList.length;
-
-            
-        //     // console.log(tempIndex);
-        //     let tempData = {            
-        //     id: '0'+tempIndex, // 组件 ID
-        //     type: "新增", // 组件类型
-        //     data: "我是新增", // 
-        //     width: "100px", // 容器宽度
-        //     height: "100px", // 容器高度
-        //     left: Math.random()*600+ '0'+"px", // 容器左边距
-        //     top: Math.random()*600+'0'+"px", // 容器上边距
-        //     }
-        //     this.dataList.push(tempData);
-        // }
+  computed: {
+  	dataListNew() {
+        return JSON.parse(JSON.stringify(this.dataList))//watch监听要求
+  	},
+ },
+  methods: {
+    // cutit(){
+    //     let temp = this.dataList;
+    //     temp.pop();
+    //     this.dataList = temp;
+    // },
+    // addit(){
+    //     let temp = this.dataList;
+    //     let tempIndex = this.dataList.length;
+    //     // console.log(tempIndex);
+    //     let tempData = {
+    //     id: '0'+tempIndex, // 组件 ID
+    //     type: "新增", // 组件类型
+    //     data: "我是新增", //
+    //     width: "100px", // 容器宽度
+    //     height: "100px", // 容器高度
+    //     left: Math.random()*600+ '0'+"px", // 容器左边距
+    //     top: Math.random()*600+'0'+"px", // 容器上边距
+    //     }
+    //     this.dataList.push(tempData);
+    // }
+  },
+  mounted() {
+    // const contain = document.getElementsByClassName("contain");
+    const contain = document.querySelector(".contain");
+    this.dataList.forEach((item) => {
+      let temp = document.createElement(`${item.type}`);
+      temp.innerHTML = `${item.data}`;
+      temp.style.color = `${item.color}`;
+      temp.style.width = `${item.width}`;
+      temp.style.height = `${item.height}`;
+      temp.style.position = "absolute";
+      temp.style.left = `${item.left}`;
+      temp.style.top = `${item.top}`;
+      temp.style.fontSize = `${item.size}`;
+      temp.style.backgroundColor = "skyblue";
+      contain.appendChild(temp);
+    });
+  },
+  watch: {
+    dataList: {
+      handler(newValue, oldValue) {
+        for (let i = 0; i < oldValue.length; i++) {
+          if (oldValue[i].id == newValue[i].id && newValue.length == oldValue.length) {
+            break;
+          } else {
+            this.dataList = newValue
+            break;
+          }
+        }
+        const contain = document.querySelector(".contain");
+        while(contain.children.length != 0){
+          contain.removeChild(contain.children[contain.children.length-1])
+        }
+        this.dataList.forEach((item) => {
+          let temp = document.createElement(`${item.type}`);
+          temp.innerHTML = `${item.data}`;
+          temp.style.color = `${item.color}`;
+          temp.style.width = `${item.width}`;
+          temp.style.height = `${item.height}`;
+          temp.style.position = "absolute";
+          temp.style.left = `${item.left}`;
+          temp.style.top = `${item.top}`;
+          temp.style.fontSize = `${item.size}`;
+          temp.style.backgroundColor = "skyblue";
+          contain.appendChild(temp);
+        });
+      },
+      immediate: false,
+      deep: true,
     },
-    mounted() {
-      // const contain = document.getElementsByClassName("contain");
-      const contain = document.querySelector(".contain")
-      this.dataList.forEach(item => {
-        let temp = document.createElement(`${item.type}`)
-        temp.innerHTML=`${item.data}`
-        temp.style.color=`${item.color}`
-        temp.style.width=`${item.width}`
-        temp.style.height=`${item.height}`
-        temp.style.position= "absolute"
-        temp.style.left=`${item.left}`
-        temp.style.top=`${item.top}`
-        temp.style.fontSize=`${item.size}`
-        temp.style.backgroundColor="skyblue"
-        contain.appendChild(temp)
-      });
-      
-    },
-}
+  },
+};
 </script>
 
 <style scope>
@@ -90,7 +123,7 @@ export default {
   backdrop-filter: blur(4px);
   overflow: hidden;
 }
-.zindex{
-    z-index: -1000;
+.zindex {
+  z-index: -1000;
 }
 </style>
--- src/views/DrawPanel/Father.vue
+++ src/views/DrawPanel/Father.vue
@@ -1,6 +1,63 @@
 <template>
   <div>
-    <DrawPanel :dataList="datasList"/>
+    <DrawPanel :dataList="datasList" />
+    <div class="contain">
+      <button
+        style="
+          width: 150px;
+          height: 50px;
+          background-color: rgb(196, 255, 205);
+          margin-left: 15px;
+        "
+        @click="addInput"
+      >
+        点我增加input
+      </button>
+      <button
+        style="
+          width: 150px;
+          height: 50px;
+          background-color: rgb(196, 255, 228);
+          margin-left: 15px;
+        "
+        @click="addText"
+      >
+        点我增加text
+      </button>
+      <button
+        style="
+          width: 150px;
+          height: 50px;
+          background-color: rgb(196, 255, 242);
+          margin-left: 15px;
+        "
+        @click="addImage"
+      >
+        点我增加image
+      </button>
+      <button
+        style="
+          width: 150px;
+          height: 50px;
+          background-color: rgb(196, 255, 258);
+          margin-left: 15px;
+        "
+        @click="addVideo"
+      >
+        点我增加video
+      </button>
+      <button
+        style="
+          width: 150px;
+          height: 50px;
+          background-color: rgb(196, 255, 278);
+          margin-left: 15px;
+        "
+        @click="deleteOne"
+      >
+        点我删除
+      </button>
+    </div>
   </div>
 </template>
 
@@ -11,7 +68,6 @@ export default {
   components: { DrawPanel },
   data() {
     return {
-        
       datasList: [
         {
           id: "01", // 组件 ID
@@ -45,7 +101,7 @@ export default {
         {
           id: "04", // 组件 ID
           type: "input", // 组件类型
-          data: "我是输入框", // 
+          data: "我是输入框", //
           width: "100px", // 容器宽度
           height: "100px", // 容器高度
           left: "300px", // 容器左边距
@@ -54,8 +110,70 @@ export default {
       ],
     };
   },
+  methods:{
+    addInput(){
+      const tempList={
+        id:toString(this.datasList.length),
+        type:"input",
+        data: "我是输入框", 
+        width: "100px", 
+        height: "100px", 
+        left: Math.random()*600+ '0'+"px", 
+        top: Math.random()*600+'0'+"px", 
+      }
+      this.datasList.push(tempList)
+    },
+    addText(){
+      const tempList={
+        id:toString(this.datasList.length),
+        type:"text",
+        data: "我是text", 
+        width: "100px", 
+        height: "100px", 
+        left: Math.random()*600+ '0'+"px", 
+        top: Math.random()*600+'0'+"px", 
+      } 
+      this.datasList.push(tempList)     
+    },
+    addImage(){
+      const tempList={
+        id:toString(this.datasList.length),
+        type:"image",
+        data: "我是图片", 
+        width: "100px", 
+        height: "100px", 
+        left: Math.random()*600+ '0'+"px", 
+        top: Math.random()*600+'0'+"px", 
+      }
+      this.datasList.push(tempList)
+    },
+    addVideo(){
+      const tempList={
+        id:toString(this.datasList.length),
+        type:"video",
+        data: "我是视频",
+        width: "100px", 
+        height: "100px", 
+        left: Math.random()*600+ '0'+"px", 
+        top: Math.random()*600+'0'+"px", 
+      }
+      this.datasList.push(tempList)
+    },
+    deleteOne(){
+      let temp = this.datasList
+      temp.pop()
+      this.datasList = temp
+    }
+  }
 };
 </script>
 
 <style>
+.contain button {
+  scale: 1;
+  transition: 0.5s;
+}
+.contain button:hover {
+  transform: scale(1.2);
+}
 </style>
